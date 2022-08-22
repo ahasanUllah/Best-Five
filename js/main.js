9937c59231af4f1b89c87c5f7ae387a943a07612
ahasanUllah <88593825+ahasanUllah@users.noreply.github.com>
2022-08-22 10:49:58 +0600

    Selected Five integrated

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log('hello js');
+
+let count = 0;
+function selected(e){
+    count++
+    const playerName = e.parentNode.children[0].innerText
+    const ol = document.getElementById('player-list')
+    if(count <= 5){
+        const li = document.createElement('li')
+    li.innerText = playerName
+    ol.appendChild(li)
+    e.disabled = true;
+    console.log(e);
+    }
+    else{
+        return alert('You Cannot select more than 5 Players')
+    }
+}
