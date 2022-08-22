163f12c9fd67634a9c17593819ab8cc18b846703
ahasanUllah <88593825+ahasanUllah@users.noreply.github.com>
2022-08-22 13:36:10 +0600

    budget section intigrated

--- js/main.js
+++ js/main.js
@@ -5,13 +5,49 @@ function selected(e){
     const playerName = e.parentNode.children[0].innerText
     const ol = document.getElementById('player-list')
     if(count <= 5){
-        const li = document.createElement('li')
+    const li = document.createElement('li')
     li.innerText = playerName
+    li.style.margin = '12px 0'
     ol.appendChild(li)
     e.disabled = true;
-    console.log(e);
     }
     else{
         return alert('You Cannot select more than 5 Players')
     }
 }
+function getInutValue(id){
+    const inputField = document.getElementById(id)
+    const inputFieldString = inputField.value;
+    const inputValue = parseInt(inputFieldString)
+    return inputValue
+}
+document.getElementById('calculate-btn').addEventListener('click', function(){
+    
+    const currentPerPlayer = getInutValue('per-player')
+
+    const liLength = document.querySelectorAll('#player-list li').length
+    if(liLength === 0){
+        return alert('please select some player')
+    }
+    else if(isNaN(currentPerPlayer)){
+        return alert('please put some number')
+    }
+    const totalPlayerExp = currentPerPlayer * liLength
+    
+    const playerExpense = document.getElementById('player-expense');
+    playerExpense.innerText = totalPlayerExp
+
+})
+document.getElementById('calculate-total-btn').addEventListener('click', function(){
+   const managerSalary = getInutValue('manager')
+   const coachSalary = getInutValue('coach')
+   const totalField = document.getElementById('total')
+   const playerExpenseString = document.getElementById('player-expense').innerText
+   const playerExpense = parseInt(playerExpenseString)
+   if(isNaN(managerSalary) || isNaN(coachSalary)){
+    return alert('put some number ')
+   }
+   const total = managerSalary + coachSalary + playerExpense
+   totalField.innerText = total
+    
+})
